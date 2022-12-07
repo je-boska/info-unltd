54ebddedba10903e2dbcab195f5c424fcae2ecac
je-boska <joneirikboska@gmail.com>
2022-12-07 17:53:15 +0100

    Add bandcamp embed

--- pages/releases/[slug].tsx
+++ pages/releases/[slug].tsx
@@ -8,7 +8,8 @@ import { renderRichTextWithImages } from '../../utils/rich-text';
 export default function Release({
   release,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { title, artist, description, artworkCollection } = release;
+  const { title, artist, description, artworkCollection, bandcampEmbed } =
+    release;
 
   const metaTitle = artist.toUpperCase() + ' - ' + title;
 
@@ -17,6 +18,12 @@ export default function Release({
       <div className='m-4 grid md:grid-cols-2 gap-4'>
         <div className='md:order-2'>
           <Carousel media={artworkCollection.items} />
+          {bandcampEmbed ? (
+            <div
+              className='mt-4 w-full'
+              dangerouslySetInnerHTML={{ __html: bandcampEmbed }}
+            ></div>
+          ) : null}
         </div>
         <div className='md:order-1 max-w-3xl text-xl md:text-2xl'>
           <h2 className='mb-6'>
