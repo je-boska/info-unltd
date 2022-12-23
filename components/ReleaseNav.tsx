b34c79bebce6e2bea61e8a6a02eea7ed84dacfcc
je-boska <joneirikboska@gmail.com>
2022-12-23 18:47:12 +0100

    Make key presses navigate between releases

--- components/ReleaseNav.tsx
+++ components/ReleaseNav.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Release } from '../types/shared';
 
 export default function ReleaseNav({
@@ -14,11 +16,38 @@ export default function ReleaseNav({
   first: Release;
   last: Release;
 }) {
+  const router = useRouter();
+
+  const nextPath = `/releases/${prev ? prev.slug : last.slug}`;
+  const prevPath = `/releases/${next ? next.slug : first.slug}`;
+
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.isComposing || e.keyCode === 39) {
+        router.push(nextPath);
+      }
+      if (e.isComposing || e.keyCode === 37) {
+        router.push(prevPath);
+      }
+    });
+
+    return () => {
+      document.removeEventListener('keydown', (e) => {
+        if (e.isComposing || e.keyCode === 39) {
+          router.push(nextPath);
+        }
+        if (e.isComposing || e.keyCode === 37) {
+          router.push(prevPath);
+        }
+      });
+    };
+  });
+
   return (
     <div className='text-lg md:text-xl lg:text-2xl flex'>
-      <Link href={`/releases/${next ? next.slug : first.slug}`}>←</Link>
+      <Link href={prevPath}>←</Link>
       <p className='px-2'>{current.catalogNumber}</p>
-      <Link href={`/releases/${prev ? prev.slug : last.slug}`}>→</Link>
+      <Link href={nextPath}>→</Link>
     </div>
   );
 }
